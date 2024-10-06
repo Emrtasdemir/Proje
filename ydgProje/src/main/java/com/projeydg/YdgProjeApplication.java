31f9b26ff013234d987cf5d48010c709e824e4c7
Emir Taşdemir <100260548+Emrtasdemir@users.noreply.github.com>
2024-10-06 17:18:54 +0300

    Add files via upload

---
+++ ydgProje/src/main/java/com/projeydg/YdgProjeApplication.java
@@ -0,0 +1,13 @@
+package com.projeydg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YdgProjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YdgProjeApplication.class, args);
+	}
+
+}
---
+++ ydgProje/src/test/java/com/projeydg/YdgProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeydg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YdgProjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
